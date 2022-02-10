c83aaab12442c553b6ca8454994f800cae1d2206
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-10 22:21:07 +0100

    Started slug logistic

---
+++ pages/post/[slug].js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { getPosts, getPostDetails} from '../../services';
+
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsFrom } from '../../components;
+
+
+const PostDetails = () => {
+  return (
+    <div>PostDetails</div>
+  )
+}
+
+export default PostDetails
